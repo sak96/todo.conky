c3cab53f8013b9af14dd1db49fc8ab003b13b1e0
sak96 <sanketkamath1996@gmail.com>
2018-09-10 18:25:26 +0530

    added listing feature for command line

--- todo/todo.sh
+++ todo/todo.sh
@@ -5,10 +5,11 @@ help(){
 	echo -e "updates todo list for conky";
 	echo -e "\t-a, add\t\tappends the argument to conky";
 	echo -e "\t-r, remove\tremoves the argument(th) number or all in todo";
+	echo -e "\t-l, list\tlists all the todo with the item number";
 	echo -e "\t-h, help\tdisplay this help and exit";	
 }
 
-if [[ "$#" -ne "2" ]]; then
+if [[ "$1" != "-l" ]] && [[ "$1" != "list" ]] && [[ "$#" -ne "2" ]]; then
 	help ;
 	exit 1;
 fi
@@ -36,6 +37,10 @@ remove(){
 	refresh;
 }
 
+list(){
+    cat -n "$HOME/.conky/todo/To-do.txt" | fmt -s -w 50
+}
+
 
 
 case $op in	
@@ -46,6 +51,9 @@ case $op in
 	"remove" | "-r") 
 		shift;
 		remove "$@";
+		;;
+	"list" | "-l")
+	    list "$@";
 		;;
 	"help" | "-h")
 		help "$@";
